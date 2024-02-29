9c97296ffb651fdb8c0b6660b83295737a5b5ffd
Patrick <patrickwspencer@gmail.com>
2024-02-29 16:59:08 -0500

    renames

--- analyze.py
+++ analyze.py
@@ -1,5 +1,5 @@
 from train_eval import eval
 from train_json_viz import create_plotly_figure
 
-# create_plotly_figure('policies/train4.json')
-eval('policies/policy4.zip', 'pterobot')
+create_plotly_figure('trained_policies/main_seq/train4.json')
+# eval('trained_policies/main_seq/policy4.zip', 'pterobot')
