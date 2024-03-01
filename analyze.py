498cbb3313596d83709a6dc3edf03e10bb210470
Patrick <patrickwspencer@gmail.com>
2024-02-29 23:45:38 -0500

    update lowvel weight

--- analyze.py
+++ analyze.py
@@ -2,6 +2,6 @@ from train_eval import eval
 from train_json_viz import create_plotly_figure, save_plotly_figure
 from pathlib import Path
 
-policy = 6
-# create_plotly_figure(f'trained_policies/main_seq/train{policy}.json', f'trained_policies/main_seq/train{policy}.html')
-eval(f'trained_policies/main_seq/policy{policy}.zip', 'pterobot')
+policy = 7
+create_plotly_figure(f'trained_policies/main_seq/train{policy}.json', f'trained_policies/main_seq/train{policy}.html')
+# eval(f'trained_policies/main_seq/policy{policy}.zip', 'pterobot')
