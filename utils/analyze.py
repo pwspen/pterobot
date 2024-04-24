3c5ade2d526f68ffc0b4fb9e582b3265a944694b
Patrick <patrickwspencer@gmail.com>
2024-04-24 17:25:32 -0400

    update terminate zrange

--- utils/analyze.py
+++ utils/analyze.py
@@ -2,6 +2,6 @@ from train_eval import eval
 from train_json_viz import create_plotly_figure, save_plotly_figure
 from pathlib import Path
 
-policy = 11
-# create_plotly_figure(f'trained_policies/main_seq/{policy}/train{policy}.json', f'trained_policies/main_seq/{policy}/train{policy}.html')
-eval(f'trained_policies/main_seq/{policy}/policy{policy}.zip', 'pterobot')
+policy = 12
+create_plotly_figure(f'trained_policies/{policy}/train{policy}.json', f'trained_policies/{policy}/train{policy}.html')
+eval(f'trained_policies/{policy}/policy{policy}.zip', 'pterobot')
--- utils/pterobot_brax_env.py
+++ utils/pterobot_brax_env.py
@@ -53,7 +53,7 @@ class Pterobot(PipelineEnv):
           use_contact_forces=False,
           contact_cost_weight=5e-4,
           terminate_when_unhealthy=True,
-          healthy_z_range=(0.15, 10),
+          healthy_z_range=(0.2, 10),
           contact_force_range=(-1.0, 1.0),
           reset_noise_scale=0.1,
           exclude_current_positions_from_observation=True,
